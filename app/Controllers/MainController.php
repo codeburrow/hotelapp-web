8d80825ddf2ca0e7442a188ca26398e4579e5715
Antony Kalogeropoulos <anthonykalogeropoulos@gmail.com>
2016-10-12 14:46:21 +0300

    Test API

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -215,6 +215,6 @@ class MainController extends Controller
 
     public function getUserIdFromPostRequest()
     {
-        echo json_decode(file_get_contents('php://input'));
+        var_dump(json_decode(file_get_contents('php://input')));
     }
 }
